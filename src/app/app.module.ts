16ac131ad3247df8c5aed53a89bac1ddd400dbcb
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-16 10:50:54 +1000

    Added two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostCreateComponent],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html',
 })
 export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
 
+  // // one-way binding
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   // console.dir(postInput); // postInput is the entire HTML textarea element node
+  //   this.newPost = postInput.value;
+  // }
+
+  // 2-way binding
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
 }
