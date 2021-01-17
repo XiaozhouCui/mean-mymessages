d5980c985b5d022531ef824abbbbbf05d56c0c88
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-17 12:15:30 +1000

    Upgraded to template-driven-form with validations

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 @Component({
   selector: 'app-post-create',
@@ -19,12 +20,20 @@ export class PostCreateComponent {
   //   this.newPost = postInput.value;
   // }
 
-  // 2-way binding
-  onAddPost() {
+  // // 2-way binding
+  // onAddPost() {
+  //   const post = { title: this.enteredTitle, content: this.enteredContent };
+  //   // send the post data in an event to parent component (app.component.html) for event binding
+  //   this.postCreated.emit(post); // "post" data will shown as $event in parent component
+  // }
+
+  // NgForm: reactive form
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
     // implement Post interface
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
     // send the post data in an event to parent component (app.component.html) for event binding
     this.postCreated.emit(post); // "post" data will shown as $event in parent component
