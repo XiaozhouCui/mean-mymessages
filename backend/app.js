5af4d9c929a06bae26aa436bd6ff8b12ec29bc4f
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-20 09:11:11 +1000

    Refactor: extracted backend routers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -20,55 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", async (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  const newPost = await post.save();
-  // console.log(newPost);
-  res.status(201).json({
-    message: "Post added successfully",
-    postId: newPost._id,
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((posts) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: posts,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found" });
-    }
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((result) => {
-    // console.log(result);
-    res.status(200).json({ message: "Update successful" });
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    // console.log(result);
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
