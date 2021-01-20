d603bc2e839ab227611e2d83c77bc3750a8a3d7b
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-20 16:55:41 +1000

    Server side image upload with multer

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const multer = require("multer");
+
 const Post = require("../models/post");
 
 const router = express.Router();
 
-router.post("", async (req, res, next) => {
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+// save posted file to disk
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // check if file type is included in MIME_TYPE_MAP
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    const error = isValid ? null : new Error("invalid mime type");
+    // destination path is relative to server.js
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+// multer(storage).single("image") will extract a fime form req.body.image
+router.post("", multer(storage).single("image"), async (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
