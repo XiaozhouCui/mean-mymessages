838b42e73ebc3519c8f514f05475b0667340fcd5
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-23 15:32:52 +1000

    Use AuthService and DI to reflect logged-in status on header

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthData } from './auth-data.model';
@@ -6,10 +7,17 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  private token: string;
+  private token: string; // store jwt from api
+  private authStatusListener = new Subject<boolean>(); // push login status to other components
 
   getToken() {
     return this.token;
+  }
+
+  // getter will only return an observable, not a subject
+  getAuthStatusListener() {
+    // asObservable: so that other components CANNOT emit new values, only this service (Subject) can emit
+    return this.authStatusListener.asObservable()
   }
 
   createUser(email: string, password: string) {
@@ -27,7 +35,8 @@ export class AuthService {
       .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
       .subscribe((response) => {
         // console.log(response); // {token: "ufjkwqnepoirvcoiu"}
-        this.token = response.token;
+        this.token = response.token; // store token to be added in auth header
+        this.authStatusListener.next(true); // emit logged-in status to other components (e.g. header)
       });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false; // store the logged-in status
+  private authListenerSubs: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
+  // Injected authService can push logged-in status to header component
   ngOnInit(): void {
+    // subscribe to the injected observable (.getAuthStatusListener()) to get logged-in status
+    this.authListenerSubs = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
   }
 
+  // unsubscribe on destroy
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
 }
