dd932ddeac99785c4d5fde7b2988e11b0592f6b5
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-23 10:16:04 +1000

    Connect Angular to backend with authService and DI

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthData } from './auth-data.model';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  createUser(email: string, password: string) {
+    const authData: AuthData = { email, password };
+    this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { AuthData } from '../auth-data.model';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -8,7 +10,12 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent {
   isLoading = false;
 
+  // Inject authService
+  constructor(public authService: AuthService) {}
+
   onSignup(form: NgForm) {
-    console.log(form.value);
+    // console.log(form.value); // { email: "blahblah", password: "blahblah" }
+    if (form.invalid) return;
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
