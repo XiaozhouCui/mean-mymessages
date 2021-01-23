eb2e05551976e8b9e6c4e8fac42a6c8425958cfa
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-23 23:23:32 +1000

    Frontend: protect routes with AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -8,8 +9,18 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  {
+    path: 'create',
+    component: PostCreateComponent,
+    // protect this route with AuthGuard
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'edit/:postId',
+    component: PostCreateComponent,
+    // protect this route with AuthGuard
+    canActivate: [AuthGuard],
+  },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
@@ -17,5 +28,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
+
+// need @Injectable() when injecting services into services
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const isAuth = this.authService.getIsAuth(); // getter fn returns a boolean
+    if (!isAuth) {
+      // if not logged in, redirect user to login page
+      this.router.navigate(['/login'])
+    }
+    return isAuth;
+  }
+}
