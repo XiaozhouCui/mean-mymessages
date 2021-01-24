2e7c509e088b1cf89a56da4a34d2f6e7d4a884f6
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-24 16:45:19 +1000

    Added Error Dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -18,7 +18,11 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        this.dialog.open(ErrorComponent);
+        let errorMessage = 'An unknown error occured!';
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
         // return an observable
         return throwError(error);
       })
