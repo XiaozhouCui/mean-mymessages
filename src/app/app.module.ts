5ef63069a81f8c2b6012f8b85691070cb0b49191
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-24 16:33:27 +1000

    Added Error Component and displayed in MatDialog modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { ErrorInterceptor } from './error-interceptor';
     PostListComponent,
     LoginComponent,
     SignupComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
@@ -53,5 +55,6 @@ import { ErrorInterceptor } from './error-interceptor';
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent],
 })
 export class AppModule {}
--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -4,15 +4,21 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { ErrorComponent } from './error/error.component';
 
+// Inject angular-material dialog as a service
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private dialog: MatDialog) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.message)
+        this.dialog.open(ErrorComponent);
         // return an observable
         return throwError(error);
       })
